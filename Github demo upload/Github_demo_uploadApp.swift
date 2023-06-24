4dc36278f29660bbd1d019982c19b12cc70df549
Omar Saifeldin <omarsaifeldin252@@gmail.com>
2023-06-24 14:43:39 +0400

    Initial Commit

---
+++ Github demo upload/Github_demo_uploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_demo_uploadApp.swift
+//  Github demo upload
+//
+//  Created by Omar Saifeldin on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Github_demo_uploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
